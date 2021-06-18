8737eaf8b212f2f1a6bad7d7b8b131ffae391e21
Alexandr Nefedzin <Demo2886@yandex.by>
2021-06-18 15:44:53 +0300

    add search version

---
+++ Search_version.ps1
@@ -0,0 +1,3 @@
+﻿$n=(Select-String -Path 'D:\BelHard_DevOps_Lessons\PowerShell_Lessons_2\dbodemo-access.log' -Pattern 'version.txt' | measure).Count
+$i='Колличество обращений в локацию /ibank/version.txt: '+ $n
+$i
