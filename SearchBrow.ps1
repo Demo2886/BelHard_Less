8f3e3bbf85e3d83aac1fa3e35da70d3a1908b535
Alexandr Nefedzin <Demo2886@yandex.by>
2021-06-18 15:54:46 +0300

    add search brouw

---
+++ SearchBrow.ps1
@@ -0,0 +1,4 @@
+﻿$LogPath = "D:\BelHard_DevOps_Lessons\PowerShell_Lessons_2\dbodemo-access.log" 
+
+$Lines = (Get-Content $LogPath | Select-String -Pattern '\w*\/\d*\.\d*' -AllMatches).Matches.Value | Select-Object -Unique
+$Lines
