99bdf963edb3ec009dd7437e23717f8b6659f662
Alexandr Nefedzin <Demo2886@yandex.by>
2021-06-18 13:07:21 +0300

    script data

---
+++ date.ps1
@@ -0,0 +1,13 @@
+﻿# Реализация скрипта вывода каждые 5 секунд дату и название имени файла.
+# Так же с передачей параметра.
+
+Param ($timeout=5)
+
+while($true){
+    $file=Get-Item "D:\BelHard_DevOps_Lessons\PowerShell_Lessons\date.ps1"
+    $Date=Get-Date -Format "yyyy-MM-dd HH:mm:ss zzzz"
+    $c=$file.Name+" "+$Date
+    $c
+    Start-Sleep -Seconds $timeout
+    }
+#}
