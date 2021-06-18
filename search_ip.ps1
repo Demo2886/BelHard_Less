140acc6ac835e1786c6f91013d6c7b2b11df8c39
Alexandr Nefedzin <Demo2886@yandex.by>
2021-06-18 15:38:54 +0300

    add coment

--- search_ip.ps1
+++ search_ip.ps1
@@ -1,5 +1,6 @@
 ﻿$LogPath = "D:\BelHard_DevOps_Lessons\PowerShell_Lessons_2\dbodemo-access.log" 
 
+#инщем уникальные IP адреса
 $IP = (Get-Content $LogPath | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value | Select-Object -Unique
 #$HostName = (Get-Content $LogPath | Select-String -Pattern "\w\.\w\.\w" -AllMatches).Matches.Value | Select-Object -Unique
 $IP
